4c1cb27888721c46f4d084ca94f7af6d3b9965c8
maximiliansl <maximilian.slapnik@gmail.com>
2020-11-11 17:42:05 +0100

    initialize app.js

--- TokenSwap/src/js/app.js
+++ TokenSwap/src/js/app.js
@@ -1,7 +1,23 @@
 App = {
+
     web3Provider: null,
     contracts: {},
 
-    // init web3
 
+    // init app
+    init: function() {
+        console.log("App initialized...")
+    },
+
+    initWeb3: function() {
+        web3.setProvider('ws://localhost:7545');
+    }
 }
+
+// whenever window loads, initialize app
+$(function() {
+    $(window).load(function() {
+        App.init();
+    })
+});
+
