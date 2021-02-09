2555c9c4c92fb40f5a7cc8c0ba5e03379768cf28
TuanUR <Tuan.Nguyen@stud.uni-regensburg.de>
2021-02-09 20:12:34 +0100

    update

--- TokenSwap/migrations/2_deploy_contract.js
+++
@@ -1,7 +0,0 @@
-var TokenSwapCoin = artifacts.require("TokenSwapCoin");
-var HashedTimelockERC20 = artifacts.require("HashedTimelockERC20");
-
-module.exports = function(deployer) {
-    deployer.deploy(TokenSwapCoin);
-    deployer.deploy(HashedTimelockERC20);
-}
--- TokenSwap/test/test.js
+++
@@ -1,7 +0,0 @@
-const HashedTimelockERC20 = artifacts.require("HashedTimelockERC20")
-const tsc = artifacts.require("TokenSwapCoin")
-
-contract("HashedTimelockERC20", accounts => {
-    Alice = accounts[1]
-    Bob = accounts[2]
-})
